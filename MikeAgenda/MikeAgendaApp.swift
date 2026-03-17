21baaa6f6eeaab9c41e7da2fa688a73b0e70b7b5
MikeWu597 <i@hyp.ink>
2026-03-17 23:39:14 +0800

    Initial Commit

---
+++ MikeAgenda/MikeAgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MikeAgendaApp.swift
+//  MikeAgenda
+//
+//  Created by MikeWu597 on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct MikeAgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
